09b96cd46b1bfca6df9aa75347b79326b967a811
Mykhailo <mykhailoozirnyi@gmail.com>
2017-01-19 20:32:15 +0200

    finished work with animation and adaptation

--- js/script.js
+++ js/script.js
@@ -2,6 +2,23 @@ jQuery(function ($) {
 
     $('#visMore').on('click', function(){
         $(".gener").addClass('little-h1');
-        $(".hiden").fadeIn(700)
+        $(".hiden").fadeIn(700);
+        /*$("#startBtn").text(Less);*/
     });
+
+
+    $('#new-gener').on('click', function(){
+        $(".new-flying").addClass('little-h1');
+        $(".hiden-fly").fadeIn(700);
+
+
+    });
+
+    $('#startBtn').on('click', function(){
+        $(".absolut").addClass('little-h1');
+        $(".hiden-start").fadeIn(700);
+
+    });
+
+
 });
