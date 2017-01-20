f4b588c800d31dc47e8a91c02b6a0c0bef790ad8
Mykhailo <mykhailoozirnyi@gmail.com>
2017-01-20 22:12:22 +0200

    wrote js for buttons

--- js/script.js
+++ js/script.js
@@ -1,22 +1,55 @@
 jQuery(function ($) {
 
     $('#visMore').on('click', function(){
-        $(".gener").addClass('little-h1');
+        $(".gener").toggleClass('little-h1');
         $(".hiden").fadeIn(700);
-        /*$("#startBtn").text(Less);*/
+
+        var togBtn = $("#visMore").text(),
+            btnText = "View more",
+            newTextBtn = "Less";
+
+        if (togBtn == btnText) {
+            $("#visMore").text(newTextBtn);
+        } else {
+            $("#visMore").text(btnText);
+            $(".hiden").fadeOut(700);
+        }
     });
+                     /*-----*/
 
 
     $('#new-gener').on('click', function(){
-        $(".new-flying").addClass('little-h1');
+        $(".new-flying").toggleClass('little-h1');
         $(".hiden-fly").fadeIn(700);
 
+        var togBtn = $("#new-gener").text(),
+            btnText = "Products",
+            newTextBtn = "Less";
 
+        if (togBtn == btnText) {
+            $("#new-gener").text(newTextBtn);
+        } else {
+            $("#new-gener").text(btnText);
+            $(".hiden-fly").fadeOut(700);
+        }
     });
 
+                    /*-----*/
+
     $('#startBtn').on('click', function(){
-        $(".absolut").addClass('little-h1');
+        $(".absolut").toggleClass('little-h1');
         $(".hiden-start").show(700);
+
+        var togBtn = $("#startBtn").text(),
+            btnText = "View more",
+            newTextBtn = "Less";
+
+        if (togBtn == btnText) {
+            $("#startBtn").text(newTextBtn);
+        } else {
+            $("#startBtn").text(btnText);
+            $(".hiden-start").fadeOut(700);
+        }
 
     });
 
